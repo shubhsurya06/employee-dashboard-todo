87932ccf47fb217ddec5dd3207f67dba3c281c29
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-10-27 18:39:01 +0530

    added dashboard module, component with lazy loading

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.scss',
-  providers: [Header]
+  providers: [Header, RouterOutlet]
 })
 export class App {
-  protected readonly title = signal('employee-dashboard-todo');
+  
 
 }
